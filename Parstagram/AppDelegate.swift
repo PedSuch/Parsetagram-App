de46ba020867a325ba6c5335be7cfe866cf24173
Pedro Suchite <pedsuch@gmail.com>
2019-04-06 11:03:17 -0700

    Initial User Stories Completed

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+
+    // Initialize Parse
+    // Set applicationId and server based on the values in the Heroku settings.
+    Parse.initialize(
+      with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+        configuration.applicationId = "myAppId"
+        configuration.server = "https://parsetagrams.herokuapp.com/parse"
+      })
+    )
     return true
   }
 
