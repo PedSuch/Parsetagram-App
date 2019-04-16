333fcdfa33b4b60a8e57c4784dedc5f418379465
Pedro Suchite <pedsuch@gmail.com>
2019-04-15 21:33:45 -0700

    Errors with MessageInputBar

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -25,6 +25,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         configuration.server = "https://parsetagrams.herokuapp.com/parse"
       })
     )
+    if PFUser.current() != nil{
+      let main = UIStoryboard(name: "Main", bundle: nil)
+      let feedNavigationController = main.instantiateViewController(withIdentifier: "FeedNavigationController")
+      window?.rootViewController = feedNavigationController
+    }
     return true
   }
 
